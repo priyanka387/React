b4bf9c4bfb49e16d75fa07250ddeecd0d9e6f4c1
priyanka387 <priyankapathak222@gmail.com>
2024-02-10 00:16:22 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+// const heading = React.createElement("h1", {id:"heading"}, "Hello world from React");
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+const parent = React.createElement("div" , {id:"parent"}, 
+    React.createElement("div", {id:"child1"}, 
+    [React.createElement("h1", {}, "my name is h1 tag"), 
+    React.createElement("h2", {}, "my name is h2 tag")]
+    ),
+    React.createElement("div", {id:"child2"}, 
+    [React.createElement("h1", {}, "my name is h1 tag"), 
+    React.createElement("h2", {}, "my name is h2 tag")]
+    ));
+
+
+// console.log(parent);
+const root1 = ReactDOM.createRoot(document.getElementById("root"))
+root1.render(parent);
